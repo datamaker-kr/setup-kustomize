fbabe06b83ae92253e2b75d25ee4664a6e293a07
Juho Hong <juho.hong@datamaker.io>
2024-02-06 13:40:21 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # setup-kustomize
+
+## Description
+
+Install and setup kustomize in your GitHub Actions workflow.
+
+## Options
+
+| Input     | Description                                   |
+|-----------|-----------------------------------------------|
+| `version` | Kustomize Version to install. Defaults to `d` |
+
+## Example Usage
+
+```yaml
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  create-deployment-branch:
+    runs-on: ubuntu-latest
+    needs:
+      - publish-image
+    steps:
+      - uses: imranismail/setup-kustomize@v2
+      - run: |
+          kustomize edit set image app:${GITHUB_SHA}
+          git add .
+          git commit -m "Set `app` image tag to `${GITHUB_SHA}`"
+          git push
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+async function run() {
+  try {
+    const kustomizeVersion = core.getInput('kustomize_version', {required: true});
+    const architecture = core.getInput('architecture', {required: true});
+    const kustomizationDirectory = core.getInput('kustomize_directory', {required: true});
+    const outputPath = core.getInput('output_kustomize_yaml_path', {required: true})
+    const kustomizePath = path.join(process.env.GITHUB_WORKSPACE, 'kustomize');
+
+    // Download Kustomize
+    await exec.exec(`curl -sLO https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/${kustomizeVersion}/kustomize_${kustomizeVersion}_${architecture}.tar.gz`);
+    await exec.exec(`tar xzf ./kustomize_${kustomizeVersion}_${architecture}.tar.gz -C ${process.env.GITHUB_WORKSPACE}`);
+
+    // Build Kustomize Configuration and process it with envsubst
+    // Save the result to a temporary file
+    const tempFile = path.join(os.tmpdir(), 'kustomize_output.yaml');
+    await exec.exec(`${kustomizePath} ${kustomizationDirectory} > ${tempFile}`);
+    await exec.exec(`cat ${tempFile} | envsubst > ${outputPath}`);
+
+    // Clean up the temporary file
+    try {
+      fs.unlinkSync(tempFile);
+    } catch (error) {
+      core.warning(`Failed to delete temporary file ${tempFile}: ${error}`);
+    }
+
+    // Set output
+    core.setOutput('kustomize_yaml_path', outputPath);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
