36ee7cf2179631fc41261347c19dbcc1ca4bf493
Juho Hong <juho.hong@datamaker.io>
2024-02-13 14:57:56 +0900

    update not to use too broad try-catch

--- dist/index.js
+++ dist/index.js
@@ -28018,59 +28018,54 @@ const commandExists = __nccwpck_require__(7895);
 
 
 async function run() {
-  try {
-    const kustomizeVersion = core.getInput('kustomize_version', {required: true});
-    const architecture = core.getInput('architecture', {required: true});
-    const kustomizationDirectory = path.join(process.env.GITHUB_WORKSPACE, core.getInput('kustomize_directory', {required: true}));
-    const outputPath = core.getInput('output_path', {required: true})
-    const kustomizePath = path.join(process.env.GITHUB_WORKSPACE, 'kustomize');
+  const kustomizeVersion = core.getInput('kustomize_version', {required: true});
+  const architecture = core.getInput('architecture', {required: true});
+  const kustomizationDirectory = path.join(process.env.GITHUB_WORKSPACE, core.getInput('kustomize_directory', {required: true}));
+  const outputPath = core.getInput('output_path', {required: true})
+  const kustomizePath = path.join(process.env.GITHUB_WORKSPACE, 'kustomize');
 
-    // Build Kustomize Configuration and process it with envsubst
-    // Save the result to a temporary file
-    const tempFile = path.join(os.tmpdir(), 'kustomize_output.yaml');
+  // Build Kustomize Configuration and process it with envsubst
+  // Save the result to a temporary file
+  const tempFile = path.join(os.tmpdir(), 'kustomize_output.yaml');
 
-    const kubectlInstalled = await commandExists('kubectl');
-    if (!kubectlInstalled) {
-      if (!fs.existsSync(kustomizePath)) {
-        // Download Kustomize
-        core.debug(`Downloading kustomize version ${kustomizeVersion} for ${architecture}`);
-        await exec.exec(`curl -sLO https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/${kustomizeVersion}/kustomize_${kustomizeVersion}_${architecture}.tar.gz`);
-        await exec.exec(`tar xzf ./kustomize_${kustomizeVersion}_${architecture}.tar.gz -C ${process.env.GITHUB_WORKSPACE}`);
-      }
-
-      core.debug(`Running command: \`${kustomizePath} build ${kustomizationDirectory} -o ${tempFile}\``);
-      await exec.exec(`${kustomizePath} build ${kustomizationDirectory} -o ${tempFile}`);
-    } else {
-      core.debug(`Running command: \`kubectl kustomize ${kustomizationDirectory} -o ${tempFile}\``);
-      await exec.exec(`kubectl kustomize ${kustomizationDirectory} -o ${tempFile}`);
+  const kubectlInstalled = await commandExists('kubectl');
+  if (!kubectlInstalled) {
+    if (!fs.existsSync(kustomizePath)) {
+      // Download Kustomize
+      core.debug(`Downloading kustomize version ${kustomizeVersion} for ${architecture}`);
+      await exec.exec(`curl -sLO https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/${kustomizeVersion}/kustomize_${kustomizeVersion}_${architecture}.tar.gz`);
+      await exec.exec(`tar xzf ./kustomize_${kustomizeVersion}_${architecture}.tar.gz -C ${process.env.GITHUB_WORKSPACE}`);
     }
 
-    await exec.exec(`bash -c "cat ${tempFile} | envsubst > ${outputPath}"`, function (error, stdout, stderr) {
-      if (error) {
-        core.setFailed(`Failed to process kustomize output with envsubst: ${error}`);
-      }
+    core.debug(`Running command: \`${kustomizePath} build ${kustomizationDirectory} -o ${tempFile}\``);
+    await exec.exec(`${kustomizePath} build ${kustomizationDirectory} -o ${tempFile}`);
+  } else {
+    core.debug(`Running command: \`kubectl kustomize ${kustomizationDirectory} -o ${tempFile}\``);
+    await exec.exec(`kubectl kustomize ${kustomizationDirectory} -o ${tempFile}`);
+  }
 
-      try {
-        fs.writeFile(outputPath, stdout);
-      } catch (error) {
-        core.setFailed(`Failed to write to ${outputPath}: ${error}`);
-      }
+  await exec.exec(`bash -c "cat ${tempFile} | envsubst > ${outputPath}"`, function (error, stdout, stderr) {
+    if (error) {
+      core.setFailed(`Failed to process kustomize output with envsubst: ${error}`);
+    }
 
-      fs.unlink(tempFile);
-    })
-
-
-    core.debug(`Setting output 'result' to ${outputPath}`)
-    core.setOutput('result', outputPath);
-
-    // Clean up the temporary file
     try {
-      fs.unlinkSync(tempFile);
+      fs.writeFile(outputPath, stdout);
     } catch (error) {
-      core.warning(`Failed to delete temporary file ${tempFile}: ${error}`);
+      core.setFailed(`Failed to write to ${outputPath}: ${error}`);
     }
+
+    fs.unlink(tempFile);
+  })
+
+  core.debug(`Setting output 'result' to ${outputPath}`)
+  core.setOutput('result', outputPath);
+
+  // Clean up the temporary file
+  try {
+    fs.unlinkSync(tempFile);
   } catch (error) {
-    core.setFailed(error.message);
+    core.warning(`Failed to delete temporary file ${tempFile}: ${error}`);
   }
 }
 
